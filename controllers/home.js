f8e656ae26ba632cfbe0278e60a4475904124022
Steven Dao <iamstevendao@users.noreply.github.com>
2017-11-12 03:15:00 +1100

    search image and show result in the homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,7 @@
+const GoogleImages = require('google-images')
+
+const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY)
+
 exports.index = (req, res) => {
   res.render('home', {
     title: 'Home'
@@ -15,7 +19,37 @@ exports.search = (req, res) => {
 
 exports.result = (req, res) => {
   // redirect to the result page
-  console.log(req.params.query)
+  resObject = {}
+  resObject.images = []
+  client.search(req.params.query, { page: 1 })
+    .then(images => {
+      images.forEach(image => {
+        resObject.images.push(image.url)
+      })
+      res.render('home', resObject)
+      // res.json(images)
+      /*
+      [{
+        "url": "http://steveangello.com/boss.jpg",
+        "type": "image/jpeg",
+        "width": 1024,
+        "height": 768,
+        "size": 102451,
+        "thumbnail": {
+          "url": "http://steveangello.com/thumbnail.jpg",
+          "width": 512,
+          "height": 512
+        }
+      }]
+       */
+    }).catch((err) => {
+      console.log(err)
+      res.end()
+    })
+  // paginate results
+  //client.search('Steve Angello', { page: 2 });
 
-  res.end()
+  // search for certain size
+  //client.search('Steve Angello', { size: 'large' });
+  //res.end()
 }
